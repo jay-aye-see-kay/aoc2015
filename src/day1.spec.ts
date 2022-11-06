b45592468193fb311d1dd1c61d84055bcbceb1ef
Jack Rose <user@jackrose.co.nz>
2022-11-06 11:00:17 +1100

    day 1 part 2

--- src/day1.spec.ts
+++ src/day1.spec.ts
@@ -1,5 +1,5 @@
 import fs from "fs/promises";
-import { part1 } from "./day1";
+import { part1, part2 } from "./day1";
 
 describe("part 1", () => {
   it.each([
@@ -18,5 +18,19 @@ describe("part 1", () => {
   it("real answer", async () => {
     const input = await fs.readFile("./src/day1.input", "utf8");
     expect(part1(input)).toEqual(280);
+  });
+});
+
+describe("part 1", () => {
+  it.each([
+    [")", 1],
+    ["()())", 5],
+  ])("sample %s", (movements, expected) => {
+    expect(part2(movements)).toEqual(expected);
+  });
+
+  it("real answer", async () => {
+    const input = await fs.readFile("./src/day1.input", "utf8");
+    expect(part2(input)).toEqual(1797);
   });
 });
--- src/day1.ts
+++ src/day1.ts
@@ -43,3 +43,38 @@ export function part1(input: string) {
     }
   }, 0);
 }
+
+/**
+--- Part Two ---
+
+Now, given the same instructions, find the position of the first
+character that causes him to enter the basement (floor -1). The first
+character in the instructions has position 1, the second character has
+position 2, and so on.
+
+For example:
+
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the
+basement?
+*/
+export function part2(input: string) {
+  const basementFloor = -1;
+  let currentFloor = 0;
+
+  for (let index = 0; index < input.length; index++) {
+    const nextMove = input[index];
+
+    if (nextMove === "(") {
+      currentFloor += 1;
+    } else if (nextMove === ")") {
+      currentFloor -= 1;
+    }
+
+    if (currentFloor === basementFloor) {
+      return index + 1;
+    }
+  }
+}
