89ef33981692bb01a00ced1f0ccd428605e26985
Jack Rose <user@jackrose.co.nz>
2022-11-12 18:06:55 +1100

    helper fn to read input files

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,32 +1,32 @@
 /// Santa was hoping for a white Christmas, but his weather machine's "snow"
 /// function is powered by stars, and he's fresh out! To save Christmas, he needs
 /// you to collect fifty stars by December 25th.
-/// 
+///
 /// Collect stars by helping Santa solve puzzles. Two puzzles will be made
 /// available on each day in the Advent calendar; the second puzzle is unlocked
 /// when you complete the first. Each puzzle grants one star. Good luck!
-/// 
+///
 /// Here's an easy puzzle to warm you up.
-/// 
+///
 /// Santa is trying to deliver presents in a large apartment building, but he can't
 /// find the right floor - the directions he got are a little confusing. He starts
 /// on the ground floor (floor 0) and then follows the instructions one character
 /// at a time.
-/// 
+///
 /// An opening parenthesis, (, means he should go up one floor, and a closing
 /// parenthesis, ), means he should go down one floor.
-/// 
+///
 /// The apartment building is very tall, and the basement is very deep; he will
 /// never find the top or bottom floors.
-/// 
+///
 /// For example:
-/// 
+///
 ///     (()) and ()() both result in floor 0.
 ///     ((( and (()(()( both result in floor 3.
 ///     ))((((( also results in floor 3.
 ///     ()) and ))( both result in floor -1 (the first basement level).
 ///     ))) and )())()) both result in floor -3.
-/// 
+///
 /// To what floor do the instructions take Santa?
 fn part1(input: &str) -> i32 {
     input.chars().fold(0, |floor, c| match c {
@@ -37,17 +37,17 @@ fn part1(input: &str) -> i32 {
 }
 
 /// --- Part Two ---
-/// 
+///
 /// Now, given the same instructions, find the position of the first
 /// character that causes him to enter the basement (floor -1). The first
 /// character in the instructions has position 1, the second character has
 /// position 2, and so on.
-/// 
+///
 /// For example:
-/// 
+///
 ///     ) causes him to enter the basement at character position 1.
 ///     ()()) causes him to enter the basement at character position 5.
-/// 
+///
 /// What is the position of the character that causes Santa to first enter the
 /// basement?
 fn part2(input: &str) -> Option<i32> {
@@ -69,7 +69,7 @@ fn part2(input: &str) -> Option<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs;
+    use crate::read_input;
 
     #[test]
     fn test_part_1_samples() {
@@ -90,7 +90,7 @@ mod tests {
 
     #[test]
     fn test_part_1_real() {
-        let input = fs::read_to_string("../inputs/day1.txt").unwrap();
+        let input = read_input(1);
         assert_eq!(part1(&input), 280);
     }
 
@@ -104,7 +104,7 @@ mod tests {
 
     #[test]
     fn test_part_2_real() {
-        let input = fs::read_to_string("../inputs/day1.txt").unwrap();
+        let input = read_input(1);
         assert_eq!(part2(&input), Some(1797));
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,11 @@
 #![allow(unused)]
 
+use std::fs;
+
 mod day1;
+
+/// helper fn to read the input file all in one
+pub fn read_input(day: i32) -> String {
+    let path = format!("../inputs/day{day}.txt");
+    fs::read_to_string(&path).expect(&format!("Could not open {path}"))
+}
